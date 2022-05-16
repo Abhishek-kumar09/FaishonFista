804839ef7fc03f15f583438c0666c08bf34e156c
Abhishek-kumar09 <abhimait1909@gmail.com>
2022-05-16 23:59:48 +0530

    Updates

---
+++ precache-manifest.269ca5fcf55957ad881dbb306321365d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e619428d92418fe520f5999910dc0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f339041308fe64aa293d",
+    "url": "/static/css/main.d7523271.chunk.css"
+  },
+  {
+    "revision": "ebdaafd17cfbd01156dd",
+    "url": "/static/js/2.2c6bb9f8.chunk.js"
+  },
+  {
+    "revision": "d7e164bc880d25710db675f58f847190",
+    "url": "/static/js/2.2c6bb9f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f339041308fe64aa293d",
+    "url": "/static/js/main.453a38f1.chunk.js"
+  },
+  {
+    "revision": "2e82f99fa524aa42995f",
+    "url": "/static/js/runtime-main.3d00a00f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
